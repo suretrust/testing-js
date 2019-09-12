4f4b02259b8e0622d7077abde4e987e2abf737ba
bellom <bellomsean@gmail.com>
2019-09-12 11:40:44 +0100

    added function/test for reverseString

---
+++ test/capitalize.test.js
@@ -0,0 +1,5 @@
+const capitalize = require("../src/capitalize");
+
+test("capitalizes the first character of a string", () => {
+    expect(capitalize('test me')).toBe("Test me");
+});
---
+++ test/reverseString.test.js
@@ -0,0 +1,5 @@
+const reverseString = require("../src/reverseString");
+
+test("should reverse a string", () => {
+    expect(reverseString("string")).toBe("gnirts");
+})
