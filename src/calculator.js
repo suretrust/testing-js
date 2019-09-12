1c35af8c010a0a2dbb32d1dc4fda7c1ac3cee04b
bellom <bellomsean@gmail.com>
2019-09-12 11:58:23 +0100

    added function/test for calculator

--- src/calculator.js
+++ src/calculator.js
@@ -1,8 +1,10 @@
 const calculator = (() => {
     const add = (a, b) => a + b;
     const substract = (a, b) => a - b;
+    const divide = (a, b) => a / b;
+    const multiply = (a, b) => a * b;
 
-    return { add, substract }
+    return { add, substract, divide, multiply }
 })();
 
 module.exports = calculator;
--- test/calculator.test.js
+++ test/calculator.test.js
@@ -4,6 +4,14 @@ test('adds 4 and 3', () => {
     expect(calculator.add(4, 3)).toBe(7);
 });
 
-test('adds 4 and 3', () => {
+test('substract 3 from 4', () => {
     expect(calculator.substract(4, 3)).toBe(1);
+});
+
+test('divide 4 by 3', () => {
+    expect(calculator.divide(4, 2)).toBe(2);
+});
+
+test('multiply 4 by 3', () => {
+    expect(calculator.multiply(4, 3)).toBe(12);
 });
