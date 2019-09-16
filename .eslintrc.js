1817fab2c4733034f7398748d1304c137aa6c4d8
Saheed <sdrfngg640@gmail.com>
2019-09-16 10:53:09 +0100

    Update devdependencies and add airbnb-base to eslint extends

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
         "commonjs": true,
         "es6": true
     },
-    "extends": "eslint:recommended",
+    "extends": "airbnb-base",
     "globals": {
         "Atomics": "readonly",
         "SharedArrayBuffer": "readonly"
